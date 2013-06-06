dd25b4a2abce2a8f4154cb5d899349e6715eac7b
Kolja Dummann <k.dummann@gmail.com>
2013-06-06 21:42:56 +0200

    add lib for adc stuff which isn't expressed in mbeddr (yet)

---
+++ lib/src/adc.c
@@ -0,0 +1,25 @@
+#include <avr/io.h>
+
+void ADC_Init(void) {
+ 
+  uint16_t result;
+  ADMUX = (1<<REFS0);    
+
+  ADCSRA = (1<<ADPS1) | (1<<ADPS0);     // Frequenzvorteiler
+  ADCSRA |= (1<<ADEN);                  // ADC aktivieren
+ 
+  ADCSRA |= (1<<ADSC);                  // eine ADC-Wandlung 
+  while (ADCSRA & (1<<ADSC) ) {         // auf Abschluss der Konvertierung warten
+  }
+
+  result = ADCW;
+}
+
+uint16_t ADC_Read( uint8_t channel )
+{
+  ADMUX = (ADMUX & ~(0x1F)) | (channel & 0x1F);
+  ADCSRA |= (1<<ADSC);            // eine Wandlung "single conversion"
+  while (ADCSRA & (1<<ADSC) ) {   // auf Abschluss der Konvertierung warten
+  }
+  return ADCW;                    // ADC auslesen und zurückgeben
+}
